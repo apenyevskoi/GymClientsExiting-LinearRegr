975f8e72672e3a1202ba351480eb16525dc38836
Artem Penevskoi <110972057+apenyevskoi@users.noreply.github.com>
2023-04-10 23:23:50 +0600

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,11 @@ outputGauge(test)
 
 for i in range(1,10):
   outputGauge(i)
+
+class Testclass():
+  
+  def __init__(self, var):
+    self.var = var
+  
+  def setVar(newvar):
+    self.var = newvar
