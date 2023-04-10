8628d2273b12b15422187700484dcca033d9c988
Artem Penevskoi <apenyevskoi@gmail.com>
2023-04-10 23:31:04 +0600

    new class in test.py

--- test.py
+++ test.py
@@ -1,14 +1,22 @@
 from main import outputGauge
+test = ''
 
 outputGauge(test)
 
 for i in range(1,10):
   outputGauge(i)
 
+class NewClass():
+  def __init__(self, tmp):
+    self.tmp = tmp
+
+  def get(self):
+    return self.tmp
+
 class Testclass():
-  
+
   def __init__(self, var):
     self.var = var
-  
+
   def setVar(newvar):
     self.var = newvar
